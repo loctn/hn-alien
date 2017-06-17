bd355571948dfafd6f33f978cc10badccb7b0ecd
Loc Nguyen <loc2000@gmail.com>
2017-06-17 14:02:30 -0700

    Add duration tests

---
+++ spec/run.js
@@ -0,0 +1,7 @@
+import Jasmine from 'jasmine';
+
+
+const jasmine = new Jasmine();
+
+jasmine.loadConfigFile('spec/support/jasmine.json');
+jasmine.execute();
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,8 +1,8 @@
-export const formatDuration = time => {
-  const seconds = Date.now() / 1000 - time;
+export const formatDuration = (time, now = Date.now() / 1000) => {
+  const seconds = now - time;
   if (seconds < 3600) return Math.floor(seconds / 60) + 'm';
-  if (seconds < 216000) return Math.floor(seconds / 3600) + 'h';
-  return Math.floor(seconds / 216000) + 'd';
+  if (seconds < 86400) return Math.floor(seconds / 3600) + 'h';
+  return Math.floor(seconds / 86400) + 'd';
 };
 
 export const formatSource = url => new URL(url || 'about:blank').hostname.replace(/^www\./i, '');
