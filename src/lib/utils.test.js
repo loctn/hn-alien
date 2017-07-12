7f7d0f94cbc3ef4045d1c590c0a68b7b48f3ffcf
Loc Nguyen <loc2000@gmail.com>
2017-07-11 22:48:12 -0700

    Migrate to Jest

---
+++ src/lib/utils.test.js
@@ -0,0 +1,25 @@
+/* global test, expect */
+import { formatDuration } from './utils';
+
+
+const SECONDS_PER_HOUR = 60 * 60;
+const SECONDS_PER_DAY = SECONDS_PER_HOUR * 24;
+const now = Date.now() / 1000;
+
+test('should return how long ago in minutes', () => {  
+  expect(formatDuration(now - 0, now)).toBe('0m');
+  expect(formatDuration(now - (SECONDS_PER_HOUR - 1), now)).toMatch(/\d+m/);
+  expect(formatDuration(now - SECONDS_PER_HOUR, now)).not.toMatch(/\d+m/);
+});
+
+test('should return how long ago in hours', () => {  
+  expect(formatDuration(now - (SECONDS_PER_HOUR - 1), now)).not.toMatch(/\d+h/);
+  expect(formatDuration(now - (SECONDS_PER_DAY - 1), now)).toMatch(/\d+h/);
+  expect(formatDuration(now - SECONDS_PER_DAY, now)).not.toMatch(/\d+h/);
+});
+
+test('should return how long ago in days', () => {  
+  expect(formatDuration(now - (SECONDS_PER_DAY - 1), now)).not.toMatch(/\d+d/);
+  expect(formatDuration(now - SECONDS_PER_DAY, now)).toMatch(/\d+d/);
+  expect(formatDuration(now - SECONDS_PER_DAY * 365 * 2, now)).toMatch(/\d+d/);
+});
